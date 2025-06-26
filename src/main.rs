2825c7275c1cf2ad5e1bb75d88283c706b4e54f1
Philip Kaminski <ph.kaminski.dev@gmail.com>
2025-06-26 09:57:36 +0100

    added entry for file creation and error handling

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,50 @@ use std::collections::HashMap;
 
 // This points to the folder in which the module is.
 mod restaurant;
-use crate::restuarant::order_food;
+use crate::restaurant::order_food;
 
 fn main() {
-    order_food()
+    let path = "lines.txt";
+
+    // Result has 2 varients Ok and Err
+    // enum Result<T, E> {
+    // Ok(T),
+    // Err(E), }
+    // Where T represents the data typeof the value returns and E
+    // the type of error
+
+    // CREATE FILE
+    let output = File::create(path);
+    // One way of handling errors
+    let mut output = match output {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem creating file : {:?}", error);
+        }
+    };
+
+    // WRITE TO FILE
+    // Another way of handling errors. Panic with custom message when error occurs.
+    write!(output, "Just some \nRandom words").expect("Failed to write to file");
+
+    // READ FILE
+    // Third way of handling errors. Unwrap panics with default message if error occurs. 
+    let input = File::open(path).unwrap();
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        println!("{}", line.unwrap());
+    }
+
+/* 
+// These are equivalent:
+let input = File::open(path).unwrap();
+
+// Same as:
+let input = match File::open(path) {
+    Ok(file) => file,
+    Err(error) => panic!("called `Result::unwrap()` on an `Err` value: {:?}", error),
+};
+*/
+
 }
--- src/scratchpad.rs
+++ src/scratchpad.rs
@@ -367,3 +367,51 @@ fn main() {
     println!("Area of Rectangle, {}", rec.area());
     println!("Area of Circle: {}", circ.area());
 }
+
+// WRITING TO FILE AND ERROR HANDLING
+
+fn main() {
+    let path = "lines.txt";
+
+    // Result has 2 varients Ok and Err
+    // enum Result<T, E> {
+    // Ok(T),
+    // Err(E), }
+    // Where T represents the data typeof the value returns and E
+    // the type of error
+
+    // CREATE FILE
+    let output = File::create(path);
+    // One way of handling errors
+    let mut output = match output {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem creating file : {:?}", error);
+        }
+    };
+
+    // WRITE TO FILE
+    // Another way of handling errors. Panic with custom message when error occurs.
+    write!(output, "Just some \nRandom words").expect("Failed to write to file");
+
+    // READ FILE
+    // Third way of handling errors. Unwrap panics with default message if error occurs. 
+    let input = File::open(path).unwrap();
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        println!("{}", line.unwrap());
+    }
+
+/* 
+// These are equivalent:
+let input = File::open(path).unwrap();
+
+// Same as:
+let input = match File::open(path) {
+    Ok(file) => file,
+    Err(error) => panic!("called `Result::unwrap()` on an `Err` value: {:?}", error),
+};
+*/
+
+}
