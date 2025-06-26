fed3a4b19f3a02bb47f739678902cf0b3e8cc1c6
Philip Kaminski <ph.kaminski.dev@gmail.com>
2025-06-26 08:59:05 +0100

    Some more notes

--- src/main.rs
+++ src/main.rs
@@ -16,39 +16,10 @@ use std::collections::HashMap;
 // Arrays mustb e of the same size.
 // Arrays must be fixed size.  
 
+// This points to the folder in which the module is.
+mod restaurant;
+use crate::restuarant::order_food;
 
 fn main() {
-    const PI: f32 = 3.141592;
-    // Shape is a _trait_ - it defines the general data types ( the contract)
-    trait Shape {
-        fn new(length: f32, width: f32) -> Self;
-        fn area(&self) -> f32;
-    }
-
-    struct Rectangle {length: f32, width: f32};
-    struct Circle {length: f32, width: f32};
-    // Rectangle and Circle are structs (concrete types) - they define the specific mechanics of e.g. how area is calculated.
-    impl Shape for Rectangle {
-        fn new(length: f32, width: f32) -> Rectangle {
-            return Rectangle{length, width};
-        }
-        fn area(&self) -> f32 {
-            return self.length * self.width;
-        }
-    }
-
-    impl Shape for Circle {
-        fn new(length: f32, width: f32) -> Circle {
-            return Circle{length, width};
-        }
-        fn area(&self) -> f32 {
-            return (self.length / 2.0).powf(2.0) * PI;
-        }
-    }
-    // rec and circ are variables; their values are instances of the struct
-    let rec: Rectangle = Shape::new(10.0, 10.0);
-    let circ: Circle = Shape::new(5.0, 5.0);
-
-    println!("Area of Rectangle, {}", rec.area());
-    println!("Area of Circle: {}", circ.area());
+    order_food()
 }
